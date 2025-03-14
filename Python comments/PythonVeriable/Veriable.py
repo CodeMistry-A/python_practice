5e929182ce02791fc1ed434000b835aa9e6d89f9
CodeMistry-A <a4mujna2000@gmail.com>
2025-03-14 06:43:35 +0530

    Initial Commit

---
+++ Python comments/PythonVeriable/Veriable.py
@@ -0,0 +1,28 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#this is casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x)
+print(y)
+print(z)
+
+#You can get the data type of a variable with the type() function.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#String variables can be declared either by using single or double quotes:
+x = "John"
+# is the same as
+x = 'John'
+
---
+++ Python comments/PythonVeriable/outputVeriable.py
@@ -0,0 +1,26 @@
+# Output Variables
+# The Python print() function is often used to output variables.
+# Example
+
+x = "Python is awesome"
+print(x)
+
+
+# In the print() function, you output multiple variables, separated by a comma:
+# and You can also use the + operator to output multiple variables:
+#Example
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+ # In the print() function, when you try to combine a string and a number with the + operator, Python will give you an error:
+x = 5
+y = "John"
+print(x + y)
+
+# The best way to output multiple variables in the print() function is to separate them with commas, which even support different data types:
+x = 5 
+y = "John"
+print(x , y)
